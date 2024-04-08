387a605656abc6d52c2b6190b23ba7054fb3057a
PierreMih <pierre.mih98@gmail.com>
2024-04-08 20:16:23 +0200

    Set DbMigrator to run according to environment

--- shared/CollectEverything.DbMigrator/Program.cs
+++ shared/CollectEverything.DbMigrator/Program.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -26,8 +28,21 @@ internal class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args)
     {
+        var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? Environments.Production;
+        
+        if (!environment.Equals(Environments.Development) && !environment.Equals(Environments.Staging))
+        {
+            environment = Environments.Production;
+        }
+
         return Host.CreateDefaultBuilder(args)
             .AddAppSettingsSecretsJson()
+            .UseEnvironment(environment)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+                config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+            })
             .ConfigureLogging((context, logging) => logging.ClearProviders())
             .ConfigureServices((hostContext, services) =>
             {
