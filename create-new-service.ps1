865686530cbe579ddbd3cd3331de957ea84e276f
PierreMih <pierre.mih98@gmail.com>
2024-08-07 15:12:13 +0200

    script create new service v3

--- create-new-service.ps1
+++ create-new-service.ps1
@@ -1,5 +1,5 @@
 $serviceNameInput = $args[0]
 $solutionName = "CollectEverything"
 $folder = $serviceNameInput.ToLower()
-abp new "$solutionName.$serviceNameInput" -t module --no-ui -o services\$folder
+abp new "$solutionName.$serviceNameInput" -t module --no-ui -o services\$folder -v 8.0.2
 dotnet sln ".\$solutionName.sln" add (Get-ChildItem -Path "services\$folder" -Recurse -Include *.csproj)
