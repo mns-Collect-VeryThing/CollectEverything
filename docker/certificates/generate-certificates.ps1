c2380883dc8e1f5ca85a4e313f9e7ad604f24975
PierreMih <pierre.mih98@gmail.com>
2024-08-08 09:45:25 +0200

    Certificats pour chaque service

--- docker/certificates/generate-certificates.ps1
+++ docker/certificates/generate-certificates.ps1
@@ -2,3 +2,7 @@ dotnet dev-certs https -ep .\CollectEverything.Administration.HttpApi.Host\Colle
 dotnet dev-certs https -ep .\CollectEverything.Gateway\CollectEverything.Gateway.pfx -p 123 --trust
 dotnet dev-certs https -ep .\CollectEverything.AuthServer\CollectEverything.AuthServer.pfx -p 123 --trust
 dotnet dev-certs https -ep .\CollectEverything.Product.HttpApi.Host\CollectEverything.Product.HttpApi.Host.pfx -p 123 --trust
+dotnet dev-certs https -ep .\CollectEverything.BlazorServer\CollectEverything.BlazorServer.pfx -p 123 --trust
+dotnet dev-certs https -ep .\CollectEverything.IdentityService.HttpApi.Host\CollectEverything.IdentityService.HttpApi.Host.pfx -p 123 --trust
+dotnet dev-certs https -ep .\CollectEverything.Projects.HttpApi.Host\CollectEverything.Projects.HttpApi.Host.pfx -p 123 --trust
+dotnet dev-certs https -ep .\CollectEverything.SaaS.HttpApi.Host\CollectEverything.SaaS.HttpApi.Host.pfx -p 123 --trust
